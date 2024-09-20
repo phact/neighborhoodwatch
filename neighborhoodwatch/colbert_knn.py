0301e6102839dbe727fb59da580ce8b7d4ba5498
phact <estevezsebastian@gmail.com>
2024-09-20 10:32:38 -0400

    smaller torch batch size to avoid cuda OOM

--- neighborhoodwatch/colbert_knn.py
+++ neighborhoodwatch/colbert_knn.py
@@ -164,7 +164,8 @@ def process_knn_computation(base_filename,
                             engine='porch'):
     if engine == 'torch':
         if initial_batch_size > 100000:
-            initial_batch_size = 10000
+            #initial_batch_size = 10000
+            initial_batch_size = 9000
     rmm.mr.set_current_device_resource(rmm.mr.PoolMemoryResource(rmm.mr.ManagedMemoryResource()))
 
     print(f"-- prepare query source table for brute-force KNN computation.")
